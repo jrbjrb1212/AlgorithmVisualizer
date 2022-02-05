58eae0c0a56559cd82dea777c2d101922e7f6696
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2022-02-05 01:39:42 -0500

    Update

--- Untill.py
+++ Untill.py
@@ -11,6 +11,8 @@ class DrawInformation:
     RED = 255, 0 ,0
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
+    SIDE_PAD = 100
+    TOP_PAD = 150
 
     def __init__(self, width, height, lst):
         self.width = width
@@ -21,4 +23,50 @@ class DrawInformation:
         self.set_list(lst)
 
     def set_list(self, lst):
-        pass
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) /len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+
+def draw(draw_info):
+    draw_info.window.fill()
+
+
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+    
+    return lst
+    
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    n = 50
+    min_val = 0
+    max_val = 100
+
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInformation(800, 600, lst)
+
+
+    while run:
+        clock.tick(60)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+
+            
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
